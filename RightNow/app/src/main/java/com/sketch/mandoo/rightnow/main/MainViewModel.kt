890746bc1ac341c1792cb5ed5ad05cb2e155dc79
KimHunJin <ysg01129@naver.com>
2018-10-10 20:47:13 +0900

    #[1] Fix
    * 버스 번호 검색
    * 버스 번호 리스트 가져오기
    * 서버 api 버그 해결

---
+++ RightNow/app/src/main/java/com/sketch/mandoo/rightnow/main/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.sketch.mandoo.rightnow.main
+
+import android.arch.lifecycle.MutableLiveData
+import com.sketch.mandoo.rightnow.base.BaseViewModel
+import com.sketch.mandoo.rightnow.network.NetworkObject
+import com.sketch.mandoo.rightnow.utils.log
+
+
+class MainViewModel :BaseViewModel(){
+    private val busId = MutableLiveData<String>()
+    private val busNumber = MutableLiveData<String>()
+    private val busFirstTime = MutableLiveData<String>()
+    private val busLastTime = MutableLiveData<String>()
+    private val busTerm = MutableLiveData<String>()
+    private val busRouteType = MutableLiveData<String>()
+
+    fun bind(serviceModel: NetworkObject.BusListModel) {
+        busId.value = serviceModel.busRouteId
+        busNumber.value = serviceModel.busRouteNm
+        busFirstTime.value = serviceModel.firstBusTm
+        busLastTime.value = serviceModel.lastBusTm
+        busTerm.value = serviceModel.term
+        busRouteType.value = serviceModel.routeType
+    }
+
+    fun getBusId() = busId
+    fun getBusNumber() = busNumber
+}
